77344b2db7b873add311d1b46f1c53c1c4f0c200
TheodorosKon <theodoroskonstadinidis@gmail.com>
2024-07-28 21:41:37 +0200

    Select dropdown main features
    Main features of the select dropdown menu have been implemented. Id, label, options, multi selection and searchable implemented.

--- src/stories/SelectDropdown.stories.tsx
+++ src/stories/SelectDropdown.stories.tsx
@@ -1,18 +1,39 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { SelectDropdown } from './SelectDropdown';
+import Select from "react-select";
 
-import SelectDropdown from './SelectDropdown.tsx';
+const options = [
+    { value: '0', label: 'Option 1' },
+    { value: '1', label: 'Long Option 1' },
+    { value: '2', label: 'Test Option 1' }
+  ]
 
-const meta: Meta<typeof SelectDropdown> = {
-    component: SelectDropdown,
-    title: 'Select Dropdown'
-}
+const meta = {
+  title: 'Form/Select',
+  component: SelectDropdown,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+  },
+  //args: { multiple: false },
+} satisfies Meta<typeof SelectDropdown>;
 
 export default meta;
-
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {
-    args: {
+export const Default: Story = {
+  args: {
+    id: 'sdd-1',
+    withSeach: false,
+    options: options,
+    multiple: false,
+    optionLabel: 'Label',
+    onChange() {
         
-    }
-}
+    },
+    outlined: false,
+    disabled: false,
+  },
+};
--- src/stories/SelectDropdown.tsx
+++ src/stories/SelectDropdown.tsx
@@ -1,7 +1,41 @@
-type Props = {};
+import React, { Key } from 'react';
+import Select from "react-select";
 
-const SelectDropdown = (props: Props) => {
-    return <select></select>;
+export interface SelectProps {
+  id: string;
+  withSeach?: boolean;
+  options: any;
+  multiple?: boolean;
+  optionLabel: string;
+  outlined: boolean;
+  disabled: boolean;
+  onChange?: () => void;
 }
 
-export default SelectDropdown;
+const indicatorSeparatorStyle = {
+    alignSelf: 'stretch',
+    marginBottom: 8,
+    marginTop: 8,
+    width: 1,
+  };
+
+export const SelectDropdown = ({
+  id = "sdd-1",
+  withSeach = false,
+  options,
+  multiple = false,
+  optionLabel,
+  ...props
+}: SelectProps) => {
+  return (    
+    <label>
+        {optionLabel}
+        <Select
+            closeMenuOnSelect={!multiple}
+            isMulti={multiple}
+            isSearchable={withSeach}
+            options={options}
+        />
+    </label>
+  );
+};
